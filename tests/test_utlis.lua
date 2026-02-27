2916c9bf0d6b6f5702fd61421c4f60bbf18b4116
Kaikacy <lukakuka735@gmail.com>
2026-02-27 19:01:38 +0400

    add utils tests

--- tests/test_utlis.lua
+++ tests/test_utlis.lua
@@ -1,28 +1,44 @@
 local h = require("tests.helper")
-local expect, eq = MiniTest.expect, MiniTest.expect.equality
-
+local M = require("omb.utils")
 local T, child = h.test_set_with_child("omb.utils")
 
 T["resolve width"] = function()
     child.o.columns = 50
-    eq(child.lua_get([[M.resolve_width(0.1)]]), 5)
-    eq(child.lua_get([[M.resolve_width(1)]]), 50)
-    eq(child.lua_get([[M.resolve_width(20)]]), 20)
+    h.eq(child.lua_get([[M.resolve_width(0.1)]]), 5)
+    h.eq(child.lua_get([[M.resolve_width(1)]]), 50)
+    h.eq(child.lua_get([[M.resolve_width(20)]]), 20)
 end
 T["resolve height"] = function()
     child.o.lines = 20
-    eq(child.lua_get([[M.resolve_height(0.1)]]), 2)
-    eq(child.lua_get([[M.resolve_height(1)]]), 20)
-    eq(child.lua_get([[M.resolve_height(10)]]), 10)
+    h.eq(child.lua_get([[M.resolve_height(0.1)]]), 2)
+    h.eq(child.lua_get([[M.resolve_height(1)]]), 20)
+    h.eq(child.lua_get([[M.resolve_height(10)]]), 10)
 end
 T["clamp"] = function()
-    eq(child.lua_get([[M.clamp(5, 3, 10)]]), 5)
-    eq(child.lua_get([[M.clamp(1, 3, 10)]]), 3)
-    eq(child.lua_get([[M.clamp(12, 3, 10)]]), 10)
-    eq(child.lua_get([[M.clamp(8, 5, 5)]]), 5)
-    expect.error(function()
+    h.eq(child.lua_get([[M.clamp(5, 3, 10)]]), 5)
+    h.eq(child.lua_get([[M.clamp(1, 3, 10)]]), 3)
+    h.eq(child.lua_get([[M.clamp(12, 3, 10)]]), 10)
+    h.eq(child.lua_get([[M.clamp(8, 5, 5)]]), 5)
+    h.expect.error(function()
         child.lua([[M.clamp(5, 10, 3)]])
     end)
+end
+T["get first dup"] = function()
+    h.eq(child.lua_get([[M.get_first_dup({1, 2, 3, 2, 4})]]), 2)
+    h.eq(child.lua_get([[M.get_first_dup({1, 2, 3, 4})]]), vim.NIL)
+end
+T["zip iter"] = function()
+    local expected = {
+        { "a", "A" },
+        { "b", "B" },
+        { "c", "C" },
+        { "d", "D" },
+    }
+    local list1 = { "a", "b", "c", "d" }
+    local list2 = { "A", "B", "C", "D" }
+    for i, elem1, elem2 in M.zip_iter(list1, list2) do
+        h.eq({ elem1, elem2 }, expected[i])
+    end
 end
 
 return T
