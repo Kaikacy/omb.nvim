196009c158a40a4db83937676cae20dd7cd9cd99
Kaikacy <lukakuka735@gmail.com>
2026-03-01 21:16:17 +0400

    WIP: rewrite source formatter to include highlighting

--- lua/omb/components/source.lua
+++ lua/omb/components/source.lua
@@ -2,8 +2,11 @@ local utils = require("omb.utils")
 
 ---@alias omb.Source.Provider fun(ctx: omb.Source.ProviderContext, my: table): list: any[]
 ---@alias omb.Source.Sorter fun(ctx: omb.Source.SorterContext, my: table): sorted: any[]
----@alias omb.Source.Format fun(ctx: omb.Source.FormatContext, my: table): fmt_item: string
+---@alias omb.Source.Format fun(ctx: omb.Source.FormatContext, my: table): fmt_items: (omb.Source.FmtItem)[]|string
 ---@alias omb.Source.Assigner fun(ctx: omb.Source.AssignerContext, my: table): assigned_keys: string[]
+
+---@alias omb.Source.FmtItem [string, string]|string (text and hl group) or just text
+---@alias omb.Source.HlRange [integer, integer, string] start column, end column (0-based, end-exclusive) and hl group
 
 ---@class omb.Source.Config
 ---@field provider omb.Source.Provider
@@ -20,19 +23,22 @@ local utils = require("omb.utils")
 
 ---@class omb.Source.AssignerContext: omb.Source.SorterContext
 ---@field formatted string[]
+--  can't use lua nil as it gets confusing in lists
+---@field highlights (omb.Source.HlRange[]|vim.NIL)[]
 
 ---@class omb.Source.FullContext: omb.Source.AssignerContext
 ---@field keys string[]
 
 ---@class omb.Source.FormatContext
 ---@field item any
+---@field index integer
 
 ---@class omb.Source
 ---@field provider omb.Source.Provider
 ---@field sorter omb.Source.Sorter
 ---@field format omb.Source.Format
 ---@field assigner omb.Source.Assigner
----@field package ctx omb.Source.PartialContext|omb.Source.FullContext
+---@field ctx omb.Source.PartialContext|omb.Source.FullContext
 local Source = {}
 
 ---@param config omb.Source.Config
@@ -53,17 +59,52 @@ function Source:new(config)
     return setmetatable(source, { __index = self })
 end
 
+function Source:_insert_fmt_pair(f, h)
+    table.insert(self.ctx.formatted, f)
+    table.insert(self.ctx.highlights, h)
+end
+
+---@param fmt_items omb.Source.FmtItem[]
+function Source:_fmt_items_to_pair(fmt_items)
+    local text = ""
+    local hl_ranges = {}
+    local curr_col = 0
+    for _, fmt_item in ipairs(fmt_items) do
+        if type(fmt_item) == "string" and #fmt_item > 0 then
+            -- without hl group this segment won't be highlighted
+            -- no need to insert anything in hl_ranges
+            text = text .. fmt_item
+        elseif type(fmt_item) == "table" and #fmt_item[1] > 0 then
+            -- fmt_item: [text, hl group]
+            text = text .. fmt_item[1]
+            table.insert(hl_ranges, { curr_col, curr_col + #fmt_item[1], fmt_item[2] }) -- start, end, hl
+        end
+        curr_col = #text
+    end
+    return text, hl_ranges
+end
+
 ---@return table
 function Source:update()
+    -- ctx is reference to self.ctx, they point to same data
+    -- similarly, user_data table is passed by reference, unless if it's reassigned in user function, then it gets copied
     local ctx = self.ctx
     ---@cast ctx omb.Source.FullContext
     local user_data = {}
 
     ctx.list = self.provider(ctx, user_data)
     ctx.list = self.sorter(ctx, user_data)
-    ctx.formatted = vim.tbl_map(function(item)
-        return self.format({ item = item }, user_data)
-    end, ctx.list)
+    ctx.formatted = {}
+    ctx.highlights = {}
+    for i, item in ipairs(ctx.list) do
+        local fmt_items = self.format({ item = item, index = i }, user_data)
+        if type(fmt_items) == "string" then
+            self:_insert_fmt_pair(fmt_items, vim.NIL)
+        elseif type(fmt_items) == "table" then
+            local text, hls = self:_fmt_items_to_pair(fmt_items)
+            self:_insert_fmt_pair(text, hls)
+        end
+    end
     ctx.keys = self.assigner(ctx, user_data)
 
     assert(#ctx.keys > 0, "no keys assigned in source")
@@ -71,7 +112,6 @@ function Source:update()
 
     assert(utils.get_first_dup(ctx.keys) == nil, "duplicate key")
 
-    self.ctx = ctx
     return user_data
 end
 
--- tests/test_source.lua
+++ tests/test_source.lua
@@ -26,29 +26,37 @@ T["get_formatted_list"] = function()
 end
 T["format function"] = function()
     local expected = {
-        { "a", "a1" },
-        { "b", "b2" },
-        { "c", "c3" },
-        { "d", "d4" },
+        -- key, item, hls (hl: start, end (0-based, end-exclusive), hl group)
+        { "1", "abcd", vim.NIL },
+        { "2", "abcdef", { { 4, 6, "Comment" } } },
+        { "3", "abcdef", { { 0, 4, "Special" }, { 4, 6, "Comment" } } },
+        { "4", "abcdef", { { 0, 4, "Special" }, { 4, 6, "Comment" } } },
     }
     local source = M:new({
         provider = function()
             return { 1, 2, 3, 4 }
         end,
         format = function(ctx)
-            return string.char(96 + ctx.item) .. tostring(ctx.item)
+            if ctx.item == 1 then
+                return "abcd"
+            elseif ctx.item == 2 then
+                return { "abcd", { "ef", "Comment" } }
+            elseif ctx.item == 3 then
+                return { { "abcd", "Special" }, { "ef", "Comment" } }
+            elseif ctx.item == 4 then
+                return { { "abcd", "Special" }, { "", "Whatever" }, { "ef", "Comment" } }
+            else
+                error("unreachable")
+            end
         end,
         assigner = function(ctx)
-            return vim.tbl_map(function(x)
-                return string.sub(x, 1, 1)
-            end, ctx.formatted)
+            return vim.tbl_map(tostring, ctx.list)
         end,
     })
     source:update()
 
-    local keys, items = source:get_formatted_list()
-    for i, key, item in require("omb.utils").zip_iter(keys, items) do
-        h.eq(expected[i], { key, item })
+    for i, key in ipairs(source.ctx.keys) do
+        h.eq(expected[i], { key, source.ctx.formatted[i], source.ctx.highlights[i] })
     end
 end
 
