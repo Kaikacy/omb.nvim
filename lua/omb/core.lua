1a86f941625c46f5d11cee284aa53de56dabf684
Kaikacy <lukakuka735@gmail.com>
2026-03-15 20:44:10 +0400

    moving to id system: core and selector

--- lua/omb/core.lua
+++ lua/omb/core.lua
@@ -4,9 +4,23 @@ local M = {}
 local state = {
     win = -1,
     ns = vim.api.nvim_create_namespace("omb-drawer"),
+    selectors = {},
+    safe_id = 0, -- unique id; shall be used for everything
 }
-
 M.state = {}
 setmetatable(M.state, { __index = state })
+
+function M.next_id()
+    state.safe_id = state.safe_id + 1
+    return state.safe_id
+end
+
+function M.register_selector(selector)
+    state.selectors[selector.id] = selector
+end
+
+function M.get_selector(id)
+    return state.selectors[id]
+end
 
 return M
--- lua/omb/selector.lua
+++ lua/omb/selector.lua
@@ -4,39 +4,22 @@ local core = require("omb.core")
 ---@field source omb.Source
 ---@field drawer omb.Drawer
 ---@field handler omb.Handler
----@field assignments omb.Source.Assignments
----@field assigned_keys omb.Source.AssignedKeys
+---@field id integer
 local Selector = {}
+
+local M = {}
 
 ---@param source_cfg omb.Source.Config
 ---@param drawer_cfg omb.Drawer.Config
 ---@param handler_cfg omb.Handler.Config
----@return omb.Selector
-function Selector:new(source_cfg, drawer_cfg, handler_cfg)
-    ---@type omb.Selector
+---@return integer selector_id
+function M.new(source_cfg, drawer_cfg, handler_cfg)
+    local id = core.next_id()
     local selector = {
-        source = require("omb.components.source"):new(source_cfg),
-        drawer = require("omb.components.drawer"):new(drawer_cfg),
-        handler = require("omb.components.handler"):new(handler_cfg),
-        assignments = {},
-        assigned_keys = {},
+        id = id,
     }
-    return setmetatable(selector, { __index = self })
+    core.register_selector(selector)
+    return id
 end
 
----@param user_data table
-function Selector:update(user_data)
-    self.source:update()
-    self.assignments, self.assigned_keys = self.source:get()
-    self.drawer:update(self.assignments, self.assigned_keys, user_data)
-end
-
----@param user_data table
-function Selector:run(user_data)
-    self:update(user_data)
-    self.drawer:display()
-    self.handler:run(self.assignments, self.assigned_keys, user_data)
-    self.drawer:hide()
-end
-
-return Selector
+return M
