601407ee416f87bd4e0062222a051dd0ac2cc084
Kaikacy <lukakuka735@gmail.com>
2026-04-15 20:30:49 +0400

    update source tests

--- tests/test_source.lua
+++ tests/test_source.lua
@@ -2,73 +2,76 @@ local h = require("tests.helper")
 local M = require("omb.components.source")
 local T = MiniTest.new_set()
 
-T["get_formatted_list"] = function()
+T["update"] = function()
     local expected = {
-        -- key, item, hls (hl: start, end (0-based, end-exclusive), hl group)
-        { "a", "1", {} },
-        { "b", "2", { { 0, 1, "Comment" } } },
-        { "c", "3", { { 0, 1, "Special" } } },
-        { "d", "4", {} },
+        -- key, value, text, hl_ranges (hl_range: start, end (0-based, end-exclusive), hl-group)
+        { "a", 1, "1", {} },
+        { "b", 2, "2 text", { { 0, 1, "Comment" } } },
+        { "c", 3, "3 text", { { 0, 1, "Special" } } },
+        { "d", 4, "4", {} },
     }
     local source = M.new({
         provider = function()
-            return { 1, 2, 3, 4 }
+            return {
+                { key = "a", value = 1 },
+                { key = "b", value = 2 },
+                { key = "c", value = 3 },
+                { key = "d", value = 4 },
+            }
         end,
-        format = function(ctx)
-            if ctx.item == 2 then
-                return { { "2", "Comment" } }
-            elseif ctx.item == 3 then
-                return { { "3", "Special" } }
+        format = function(item)
+            if item.value == 2 then
+                return { { "2 text", "Comment" } }
+            elseif item.value == 3 then
+                return { { "3 text", "Special" } }
             end
-            return tostring(ctx.item)
-        end,
-        assigner = function()
-            return { "a", "b", "c", "d" }
+            return tostring(item.value)
         end,
     })
     source:update({})
 
-    local keys, items, hls = source.ctx.keys, source.ctx.formatted, source.ctx.highlights
-    for i, key, item, hl in require("omb.utils").zip_iter3(keys, items, hls) do
-        h.eq(expected[i], { key, item, hl })
+    for i, item in ipairs(source.items) do
+        h.eq(expected[i], { item.key, item.value, item.text, item.hl_ranges })
     end
 end
-T["format function"] = function()
+T["format"] = function()
     local expected = {
-        -- key, item, hls (hl: start, end (0-based, end-exclusive), hl group)
-        { "1", "abcd", {} },
-        { "2", "abcdef", {} },
-        { "3", "abcdef", { { 4, 6, "Comment" } } },
-        { "4", "abcdef", { { 0, 4, "Special" }, { 4, 6, "Comment" } } },
-        { "5", "abcdef", { { 0, 4, "Special" }, { 4, 6, "Comment" } } },
+        -- text, hl_ranges (hl_range: start, end (0-based, end-exclusive), hl-group)
+        { "abcd", {} },
+        { "abcdef", {} },
+        { "abcdef", { { 4, 6, "Comment" } } },
+        { "abcdef", { { 0, 4, "Special" }, { 4, 6, "Comment" } } },
+        { "abcdef", { { 0, 4, "Special" }, { 4, 6, "Comment" } } },
     }
     local source = M.new({
         provider = function()
-            return { 1, 2, 3, 4, 5 }
+            local items = {}
+            for i = 1, 5 do
+                items[i] = { key = tostring(i), value = i }
+            end
+            return items
         end,
-        format = function(ctx)
-            if ctx.item == 1 then
+        format = function(item, i)
+            h.eq(item.value, i)
+            if item.value == 1 then
                 return "abcd"
-            elseif ctx.item == 2 then
-                return { "abcd", "ef" }
-            elseif ctx.item == 3 then
-                return { "abcd", { "ef", "Comment" } }
-            elseif ctx.item == 4 then
+            elseif item.value == 2 then
+                return { { "abcd" }, { "ef" } }
+            elseif item.value == 3 then
+                return { { "abcd" }, { "ef", "Comment" } }
+            elseif item.value == 4 then
                 return { { "abcd", "Special" }, { "ef", "Comment" } }
-            elseif ctx.item == 5 then
+            elseif item.value == 5 then
                 return { { "abcd", "Special" }, { "", "Whatever" }, { "ef", "Comment" } }
             else
                 error("unreachable")
             end
         end,
-        assigner = function(ctx)
-            return vim.tbl_map(tostring, ctx.list)
-        end,
     })
     source:update({})
 
-    for i, key in ipairs(source.ctx.keys) do
-        h.eq(expected[i], { key, source.ctx.formatted[i], source.ctx.highlights[i] })
+    for i, item in ipairs(source.items) do
+        h.eq(expected[i], { item.text, item.hl_ranges })
     end
 end
 
